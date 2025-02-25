67709eb236654d47db59e181b415816498fa1d04
Sebastián Butcovich <sebabutcovich@gmail.com>
2025-02-25 10:20:34 -0300

    Actualización

--- View/src/Pages/dashboard/blockTotals/BlockTotal.jsx
+++ View/src/Pages/dashboard/blockTotals/BlockTotal.jsx
@@ -48,7 +48,7 @@ function BlockTotal() {
       });
     }      
   useEffect(() => {
-    setTimeout(()=>{getTotals()}, 3000);
+    setTimeout(()=>{getTotals()}, 2300);
     context.setIsUpdate(false);
   }, [context.isUpdate]);
   return (
